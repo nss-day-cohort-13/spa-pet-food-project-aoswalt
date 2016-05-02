961f28bbb960e4684da005302ca878a1a671f007
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-02 09:44:21 -0500

    Successful loading of JSON files

--- food.js
+++ food.js
@@ -1,5 +1,23 @@
 var Food = (function(food) {
   //TODO(adam): show info on page
+  var dogFood;
+  var catFood;
+
+  var dogXhr = new XMLHttpRequest();
+  dogXhr.addEventListener("load", function() {
+    dogFood = JSON.parse(this.responseText);
+    console.log("dog", dogFood);
+  });
+  dogXhr.open("GET", "dog-food.json");
+  dogXhr.send();
+
+  var catXhr = new XMLHttpRequest();
+  catXhr.addEventListener("load", function() {
+    catFood = JSON.parse(this.responseText);
+    console.log("cat", catFood);
+  });
+  catXhr.open("GET", "cat-food.json");
+  catXhr.send();
 
   return food;
 }(Food || {}));
