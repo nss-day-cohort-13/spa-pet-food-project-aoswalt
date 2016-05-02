139951a3e82514845675e973804a487041c5de75
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-02 10:45:00 -0500

    Move insert function inside iife and remove unneeded variables

--- food.js
+++ food.js
@@ -4,46 +4,42 @@
 
 var Food = (function(food) {
   var targetElement = document.getElementById("food-table-body");
-  var dogFood;
-  var catFood;
 
   var dogXhr = new XMLHttpRequest();
   dogXhr.addEventListener("load", function() {
-    dogFood = JSON.parse(this.responseText);
-    insertFood(targetElement, dogFood);
+    var data = JSON.parse(this.responseText);
+    insertFood(targetElement, data);
   });
   dogXhr.open("GET", "dog-food.json");
   dogXhr.send();
-
   var catXhr = new XMLHttpRequest();
   catXhr.addEventListener("load", function() {
-    catFood = JSON.parse(this.responseText);
-    insertFood(targetElement, catFood);
+    var data = JSON.parse(this.responseText);
+    insertFood(targetElement, data);
   });
   catXhr.open("GET", "cat-food.json");
   catXhr.send();
 
+  function insertFood(target, foodData) {
+    var brandList = foodData[Object.keys(foodData)[0]];
+    var html = "";
+
+    brandList.forEach(function(brand) {
+      html += `<tr><td>${brand.name}</td>`;
+
+      let priceArray = [];
+      brand.types.forEach(function(type) {
+        type.volumes.forEach(function(vol) {
+          priceArray.push(`${type.type}, ${vol.name}, ${vol.price}`);
+        });
+      });
+      html += `<td>${priceArray.join(" : ")}</td>`;
+
+      html += "</tr>";
+    });
+
+    target.innerHTML += html;
+  }
+
   return food;
 }(Food || {}));
-
-function insertFood(target, foodData) {
-  console.log("foodData", foodData);
-  var brandList = foodData[Object.keys(foodData)[0]];
-  var html = "";
-
-  brandList.forEach(function(brand) {
-    html += `<tr><td>${brand.name}</td>`;
-
-    let priceArray = [];
-    brand.types.forEach(function(type) {
-      type.volumes.forEach(function(vol) {
-        priceArray.push(`${type.type}, ${vol.name}, ${vol.price}`);
-      });
-    });
-    html += `<td>${priceArray.join(" : ")}</td>`;
-
-    html += "</tr>";
-  });
-
-  target.innerHTML += html;
-}
